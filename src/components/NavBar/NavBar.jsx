e71cc27979e21f19f9471892cef0275ad372aa9e
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-10-01 10:03:44 +0500

    Changed icons

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+function NavBar() {
+  return (
+    <nav>
+      <div className="categories">
+        <button>Фильмы</button>
+        <button>Сериалы</button>
+        <button>Мультфильмы</button>
+      </div>
+      {/* <Search />
+      <ImageButton /> */}
+    </nav>
+  );
+}
+
+export default NavBar;

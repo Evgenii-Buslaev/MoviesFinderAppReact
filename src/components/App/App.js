f9789e87a34b7cfb8868eb1d8183f882b6d98c2e
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-10-01 11:04:36 +0500

    Added logo and butttons to NavBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,14 @@ import "./App.css";
 import "../../css/page.css";
 import "../../css/reset.css";
 
+import NavBar from "../NavBar/NavBar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,17 @@
+import styles from "./NavBar.module.css";
+import logo from "../../icons/logo/logo.png";
+
 function NavBar() {
   return (
-    <nav>
-      <div className="categories">
-        <button>Фильмы</button>
-        <button>Сериалы</button>
-        <button>Мультфильмы</button>
+    <nav className={styles.navBarCont}>
+      <div className={styles.logoCont}>
+        <img src={logo} alt={logo}></img>
+        <h1>MOVIES</h1>
+      </div>
+      <div className={styles.categories}>
+        <button className={styles.navBtn}>Фильмы</button>
+        <button className={styles.navBtn}>Сериалы</button>
+        <button className={styles.navBtn}>Мультфильмы</button>
       </div>
       {/* <Search />
       <ImageButton /> */}

42e8ab8dda0fad752a331441df185dd947bfb1bc
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-10-06 17:04:22 +0500

    Fixed bag with showing items btwn 0 and last

--- src/API/FilmsService.js
+++ src/API/FilmsService.js
@@ -1,10 +1,13 @@
 const PREMIERS_URL =
   "https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2022&month=OCTOBER";
 
+const KEY = "8793b375-41ed-45e8-99f2-dddaf5296a1c";
+/* const KEY_RESERVE = "986eeea7-968e-4022-9877-e84daf01fcc7"; */
+
 const headers = {
   method: "GET",
   headers: {
-    "X-API-KEY": "986eeea7-968e-4022-9877-e84daf01fcc7",
+    "X-API-KEY": KEY,
     "Content-Type": "application/json",
   },
 };

319aac0bdad071d0941ba44c74aada9d12e834a4
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-10-06 11:07:56 +0500

    Created FilmsService and method to get premiers

---
+++ src/API/FilmsService.js
@@ -0,0 +1,18 @@
+const PREMIERS_URL =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2022&month=OCTOBER";
+const headers = {
+  method: "GET",
+  headers: {
+    "X-API-KEY": "986eeea7-968e-4022-9877-e84daf01fcc7",
+    "Content-Type": "application/json",
+  },
+};
+
+export default class FilmsService {
+  static async getPremiers() {
+    const response = await fetch(PREMIERS_URL, headers);
+    const res = await response.json();
+    console.log(res);
+    return res;
+  }
+}

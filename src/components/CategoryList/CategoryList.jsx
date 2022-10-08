c9861d3113fb57d3628a74e91d0905d7aedf1042
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-10-08 14:15:15 +0500

    Added dynamical pagination to CategoryList

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -2,14 +2,50 @@ import { useState, useEffect, useContext } from "react";
 
 import InlineList from "../InlineList/InlineList";
 import PacketList from "../PacketList/PacketList";
+/* import { getItems } from "../../handlers/getItems"; */
 import { AppContext } from "../../utils/context";
 import styles from "./CategoryList.module.css";
 
 function CategoryList({ filter, category, width }) {
+  const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
+
   const { appElem } = useContext(AppContext);
 
-  const scrollHandler = () => console.log(1);
+  useEffect(() => {
+    if (isLoading) {
+      console.log("fetch...");
+      fetch(
+        `https://kinopoiskapiunofficial.tech/api/v2.2/films?order=RATING&type=FILM&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000&page=${page}`,
+        {
+          method: "GET",
+          headers: {
+            "X-API-KEY": "8793b375-41ed-45e8-99f2-dddaf5296a1c",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          console.log(result);
+          setList([...list, ...result.items]);
+          setPage((prevPage) => prevPage + 1);
+        })
+        .catch((error) => console.log(error))
+        .finally(() => setIsLoading(false));
+    }
+  }, [isLoading, page]);
+
+  const scrollHandler = (e) => {
+    if (
+      e.target.scrollHeight - (e.target.scrollTop + window.innerHeight) <
+      100
+    ) {
+      console.log("scroll");
+      setIsLoading(true);
+    }
+  };
 
   useEffect(() => {
     const ref = appElem.current;
@@ -26,7 +62,7 @@ function CategoryList({ filter, category, width }) {
         filter={filter}
       />
       <h1>{category}</h1>
-      <InlineList page={page} filter={filter} />
+      <InlineList isLoading={isLoading} list={list} />
     </div>
   );
 }
--- src/components/InlineList/InlineList.jsx
+++ src/components/InlineList/InlineList.jsx
@@ -1,40 +1,27 @@
-import { useState, useEffect } from "react";
-
 import InlineListItem from "../InlineListItem/InlineListItem";
 import RouterLink from "../../UI/RouterLink/RouterLink";
 import Loader from "../../UI/Loader/Loader";
-import { getItems } from "../../handlers/getItems";
 
 import Select from "../../UI/Select/Select";
 import styles from "./InlineList.module.css";
 
-function InlineList({ page, filter }) {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    getItems(page, filter, setList, setIsLoading);
-    return setIsLoading(true);
-  }, [page, filter]);
-
+function InlineList({ filter, list }) {
   return (
     <>
       <Select />
       <div className={styles.list} type={filter}>
-        {isLoading ? (
-          <Loader />
-        ) : (
-          list.items.map((film) => (
-            <RouterLink path="/id" key={film.kinopoiskId}>
-              <InlineListItem
-                image={film.posterUrlPreview}
-                name={film.nameRu || film.nameOriginal}
-                countryList={film.countries}
-                year={film.year}
-              />
-            </RouterLink>
-          ))
-        )}
+        {list
+          ? list.map((film) => (
+              <RouterLink path="/id" key={film.kinopoiskId}>
+                <InlineListItem
+                  image={film.posterUrlPreview}
+                  name={film.nameRu || film.nameOriginal}
+                  countryList={film.countries}
+                  year={film.year}
+                />
+              </RouterLink>
+            ))
+          : null}
       </div>
     </>
   );

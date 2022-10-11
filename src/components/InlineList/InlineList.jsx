6a74c8fd7990fe5abb37c8e35da12581ee5e96c7
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-10-11 12:52:34 +0500

    Added rating to InlineItem

--- src/components/InlineList/InlineList.jsx
+++ src/components/InlineList/InlineList.jsx
@@ -18,6 +18,7 @@ function InlineList({ list, options, sort, change }) {
                 name={film.nameRu || film.nameOriginal}
                 countryList={film.countries}
                 year={film.year}
+                rating={film.ratingKinopoisk}
               />
             </RouterLink>
           ))
--- src/components/InlineListItem/InlineListItem.jsx
+++ src/components/InlineListItem/InlineListItem.jsx
@@ -1,6 +1,7 @@
 import styles from "./InlineListItem.module.css";
+import star from "../../icons/inline/star.png";
 
-function InlineListItem({ image, name, countryList, year }) {
+function InlineListItem({ image, name, countryList, year, rating }) {
   return (
     <div className={styles.item}>
       <div style={{ width: "auto" }}>
@@ -9,10 +10,14 @@ function InlineListItem({ image, name, countryList, year }) {
       <div style={{ width: "25vw" }}>
         <h2>{name}</h2>
       </div>
-      <div style={{ width: "35vw" }}>
+      <div style={{ width: "30vw" }}>
         {countryList.map((country) => (
           <h2 key={country.country}>{country.country}</h2>
         ))}
+      </div>
+      <div className={styles.ratingCont}>
+        <img className={styles.rating} src={star} alt="rating"></img>
+        <h2>{rating.toString().length > 1 ? rating : `${rating}.0`}</h2>
       </div>
       <div style={{ width: "auto" }}>
         <h2>{year}</h2>

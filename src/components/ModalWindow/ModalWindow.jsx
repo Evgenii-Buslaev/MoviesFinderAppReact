8bfe4c611e929fa0c9b615fde73c39fbe72a2408
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-10-20 18:31:45 +0500

    Added styles, responsive design for ModalWindow

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,8 +1,10 @@
+import close from "../../icons/modal/close.png";
 import styles from "./ModalWindow.module.css";
 
 function ModalWindow() {
   return (
     <div className={styles.window}>
+      <img src={close} alt="close"></img>
       <h2> Свайпайте, чтобы смотреть все премьеры </h2>
     </div>
   );
